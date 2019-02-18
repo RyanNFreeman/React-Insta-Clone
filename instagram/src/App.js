94d0cb464d0fe5f62a08a02fe2f2bd8cae9a38f8
Home <home@Ryans-MacBook-Air-8.local>
2019-02-18 16:45:00 -0700

    a little more progress. Having issues with font-awesome

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer'
 import CommentSection from './components/CommentSection/CommentSection'
+import dummyData from './dummy-data'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: dummyData
+    };
+  }
+
   render() {
     return (
       <div className="App">
           <SearchBar />
-          <PostContainer />
+          <PostContainer comments={this.state.dummyData}/>
           <CommentSection />
   
       </div>
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import IGLogo from '../../assets/iglogo.png';
+import CameraLogo from '../../assets/camera-logo.svg'
+import './SearchBar.css'
+
 
 const SearchBar = () => {
     return (
-    <div>
-        <h1>Search Bar</h1>
+    <div className='wrapper'>
+        <div className='camera-logo'>
+            <img alt="camera logo" src={CameraLogo} className="logo-image" />
+        </div>
+        <div className='image-content'>
+            <img alt="instagram logo" src={IGLogo} className="logo-image" />
+        </div>
+        <div className='input-wrapper'>
+            <input className='input' type="text" placeholder="Search" />
+        </div>
+        <div className="social-wrapper">
+            <div className="social">
+                <i className="fa fa-compass" />
+            </div>
+            <div className="social">
+                <i className="fa fa-heart" />
+            </div>
+            <div className="social">
+                <i className="fa fa-user-circle" />
+            </div>
+        </div>
     </div>
     )
 }
